8c3e755e9594f8c102e87eb0b475b0713f08eb9f
conflatexd <169180091+conflatexd@users.noreply.github.com>
2024-06-22 14:53:35 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    token : "",
-    logChannelID : "",
-    categoryId : "",
+    token : "MTI1NDAzODg3OTE5MTk2MTc0MA.GTqNQs.xD7ypuGGx_As9YM3eJtJu728Y5FMCT8MDXw9F8",
+    logChannelID : "1242795172367040532",
+    categoryId : "1160514177635467345",
     prefix : "+"
 };
